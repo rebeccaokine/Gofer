4d63235db176178d37a99facc4d87475dde8efbf
Rebecca Okine <rebeccaokine.na@gmail.com>
2023-07-08 16:30:28 -0700

    added a homescreen

---
+++ Home.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import NavigationBar from '../../components/navigationBar';
+import SearchButton from '../../components/searchButton';
+import Categories from '../../components/categories';
+import { Ionicons } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          marginVertical: 20,
+          marginHorizontal: 20,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+          }}>
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: 600,
+            }}>
+            Hello, Naa
+          </Text>
+
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Onboarding2');
+              }}
+              style={{
+                marginLeft: 140,
+              }}>
+              <Ionicons
+                name="notifications-circle-outline"
+                size={35}
+                color="black"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            fontSize: 18,
+            fontWeight: 600,
+          }}>
+          <Octicons name="location" size={20} color="black" />
+          <Text> Accra, Ghana </Text>
+        </View>
+
+        <SearchButton placeholder="Find an errand" />
+
+        <View
+          style={{
+            flex: 2,
+          }}>
+          <Text
+            style={{
+              fontWeight: 600,
+              fontSize: 22,
+              marginTop: 15,
+              marginBottom: 10,
+            }}>
+            {' '}
+            Categories{' '}
+          </Text>
+
+          <Categories />
+        </View>
+      </View>
+
+      <NavigationBar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F8EBD3',
+    fontFamily: 'Poppins',
+  },
+});
+
+export default Home;
