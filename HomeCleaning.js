82552999a2df8136a1f10adebb89422a50a9190a
Rebecca Okine <rebeccaokine.na@gmail.com>
2023-07-10 14:23:28 -0700

    added homecleaning category

---
+++ HomeCleaning.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import NavigationBar from '../components/navigationBar';
+import HomeCleaningErrands from '../components/homeCleaningErrands';
+import { AntDesign } from '@expo/vector-icons';
+
+const HomeCleaning = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          marginVertical: 20,
+          marginHorizontal: 20,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              marginRight: 40,
+            }}>
+            <AntDesign name="leftcircleo" size={37} color="black" />
+          </TouchableOpacity>
+          <Text
+            style={{
+              fontSize: 32,
+              color: 'black',
+              marginBottom: 30,
+              textAlign: 'center',
+              fontWeight: 600,
+            }}>
+            Home Cleaning
+          </Text>
+        </View>
+        <HomeCleaningErrands />
+      </View>
+
+      <NavigationBar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F8EBD3',
+    fontFamily: 'Poppins',
+  },
+});
+
+export default HomeCleaning;
