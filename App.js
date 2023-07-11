0e9be2b2e114f7569d9b369868916eebb894ebc4
Rebecca Okine <rebeccaokine.na@gmail.com>
2023-07-11 14:30:12 -0700

    update

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { View } from 'react-native';
 import {NavigationContainer}from'@react-navigation/native';
 import {createStackNavigator}from'@react-navigation/stack';
-
 import SplashScreen from "./screens/SplashScreen";
 import Onboarding1 from "./screens/Onboarding1";
 import Onboarding2 from "./screens/Onboarding2";
@@ -17,6 +16,8 @@ import ResetPassword from "./screens/ResetPassword";
 import Home from './screens/Gofer/Home';
 import HomeCleaning from './screens/HomeCleaning';
 import UpcomingSchedule from './screens/UpcomingSchedule';
+import CompleteTask from './screens/CompleteTask';
+import RateHirer from './screens/RateHirer';
 
 export default function App() {
   const MainNavigator = createStackNavigator();
@@ -26,7 +27,7 @@ export default function App() {
       <NavigationContainer>
          <MainNavigator.Navigator screenOptions={{ headerShown: false }} >
 
-          <MainNavigator.Screen name="UpcomingSchedule" component={UpcomingSchedule}/>
+          
           <MainNavigator.Screen name="SplashScreen" component={SplashScreen} />
           <MainNavigator.Screen name="Onboarding1" component={Onboarding1} />
           <MainNavigator.Screen name="Onboarding2" component={Onboarding2} />
@@ -39,6 +40,10 @@ export default function App() {
           <MainNavigator.Screen name="ResetPassword" component={ResetPassword}/>
           <MainNavigator.Screen name="Home" component={Home}/>
           <MainNavigator.Screen name="HomeCleaning" component={HomeCleaning}/>
+          <MainNavigator.Screen name="UpcomingSchedule" component={UpcomingSchedule}/>
+          <MainNavigator.Screen name="CompleteTask" component={CompleteTask}/>
+          <MainNavigator.Screen name="RateHirer" component={RateHirer}/>
+
           
 
          </MainNavigator.Navigator>
