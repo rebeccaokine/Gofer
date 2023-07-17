95f89127b6bbd9920eabfc13fdb4b7c8aa34c87f
Rebecca Okine <rebeccaokine.na@gmail.com>
2023-07-17 15:02:10 -0700

    update login screen

--- GoferLogin.js
+++ GoferLogin.js
@@ -1,185 +1,241 @@
 import React, { useState } from 'react';
-import {View, Text, TouchableOpacity, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  Pressable
+} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
-import FormTextInput from '../components/formTextInput';
-import TextLink from '../components/textLink';
-import PasswordInput from '../components/passwordInput';
+import FormTextInput from '../../components/formTextInput';
+import PasswordInput from '../../components/passwordInput';
 
-const GoferLogin  = ({navigation}) => {
+const GoferLogin = ({ navigation }) => {
   const [activeOption, setActiveOption] = useState('gofer');
 
   const handleToggle = () => {
     if (activeOption === 'gofer') {
       setActiveOption('hirer');
     } else {
-      setActiveOption('gofer')}
-  }
+      setActiveOption('gofer');
+    }
+  };
 
-  return(
+  const handleLogin = () => {
+    if (activeOption === 'gofer') {
+      navigation.navigate('Home');
+    } else {
+      navigation.navigate('HirerHome');
+    }
+  };
+
+  return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.toggleContainer}>
-      <TouchableOpacity
-        style={[
-          styles.button,
-          activeOption === 'gofer' && styles.activeButton,
-        ]}
-        onPress={handleToggle}
-      >
-        <Text style={[styles.buttonText, activeOption === 'gofer' && styles.activeButtonText]}>
-          Gofer
+      <View style={styles.toggleContainer}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            activeOption === 'gofer' && styles.activeButton,
+          ]}
+          onPress={handleToggle}>
+          <Text
+            style={[
+              styles.buttonText,
+              activeOption === 'gofer' && styles.activeButtonText,
+            ]}>
+            Gofer
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            activeOption === 'hirer' && styles.activeButton,
+          ]}
+          onPress={handleToggle}>
+          <Text
+            style={[
+              styles.buttonText,
+              activeOption === 'hirer' && styles.activeButtonText,
+            ]}>
+            Hirer
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: '500',
+            marginHorizontal: 30,
+            marginTop: 40,
+            marginBottom: 30,
+            textAlign: 'center',
+          }}>
+          Login
         </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[
-          styles.button,
-          activeOption === 'hirer' && styles.activeButton,
-        ]}
-        onPress={handleToggle}
-      >
-        <Text style={[styles.buttonText, activeOption === 'hirer' && styles.activeButtonText]}>
-          Hirer
-        </Text>
-      </TouchableOpacity>
-    </View>
+      </View>
 
-    <View>
-    <Text 
-    style={{
-     fontSize: 32,
-     fontWeight: '500',
-     marginHorizontal: 30,
-     marginTop: 40,
-     marginBottom: 30,
-     textAlign: 'center',
-    }}>
-     Login</Text>
-    </View>
+      <View>
+        <FormTextInput placeholder="Email" />
+        <PasswordInput placeholder="Password" />
+      </View>
 
-    <View>
-      <FormTextInput 
-       placeholder="Email" 
-      />
-      <PasswordInput 
-       placeholder="Password"
-      />
-    </View>
- 
-    <View style={{
-        marginHorizontal: 50,
-        textAlign: 'right',
-      }}>
-     <TextLink text=" Forgot Password?" onPress={()=> navigation.navigate("ForgotPassword")} />
-    </View>
-
-      <TouchableOpacity 
-        onPress={()=>{navigation.navigate("Onboarding2")}}
+      <View
         style={{
-          padding: 15, 
+          flexDirection: 'row',
+          marginHorizontal: 50,
+          alignSelf: 'flex-end',
+          fontWeight: '500',
+        }}>
+      <Pressable
+          onPress={() => {
+            navigation.navigate('ForgotPassword');
+          }}>
+          <Text
+            style={{
+             fontSize: 16,
+            textAlign: 'center',
+            marginTop: 10,
+            }}>
+           {' '} Forgot Password?
+          </Text>
+        </Pressable>
+      </View>
+
+       <TouchableOpacity
+        onPress={handleLogin}
+        style={{
+          padding: 15,
           backgroundColor: '#F8EBD3',
-          borderRadius: 30, 
-          borderWidth: 2, 
-          borderColor: 'black', 
-          justifyContent: 'center', 
-          alignItems: 'center',  
-          fontWeight: 800,       
+          borderRadius: 30,
+          borderWidth: 2,
+          borderColor: 'black',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontWeight: '500',
           marginTop: 10,
           marginHorizontal: 40,
-          }}
-          >
-          <Text 
+        }}>
+        <Text
           style={{
-            fontSize:20,
-          }}
-          >Login</Text>
+            fontSize: 20,
+          }}>
+          Login
+        </Text>
       </TouchableOpacity>
-      
-      <Text 
-      style={{
-        fontSize: 16,
-        color: 'gray',
-        textAlign: 'center',
-        marginTop: 20,
-      }}>
-      Don't have an account?
-      <TextLink text=" Sign Up" onPress={()=> navigation.navigate("GoferSignup")} />
-      </Text>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <Text
+          style={{
+            fontSize: 16,
+            color: 'gray',
+            textAlign: 'center',
+            marginTop: 20,
+          }}>
+          Don't have an account?
+        </Text>
+        <Pressable
+          onPress={() => {
+            navigation.navigate('GoferSignup');
+          }}>
+          <Text
+            style={{
+             fontSize: 16,
+            textAlign: 'center',
+            marginTop: 20,
+            }}>
+           {' '} Sign Up
+          </Text>
+        </Pressable>
+      </View>
 
       <Text
-      style={{
-        fontSize: 16,
-        color: 'gray',
-        marginTop: 60,
-        textAlign: 'center',
-      }}>
-       Or connect with
+        style={{
+          fontSize: 16,
+          color: 'gray',
+          marginTop: 60,
+          textAlign: 'center',
+          fontWeight: '500',
+        }}>
+        Or connect with
       </Text>
 
-     <View style={{ flexDirection: "row", marginHorizontal: 40, marginTop: 15,  justifyContent: 'center',}}>
-      <TouchableOpacity
-      onPress={()=>{navigation.navigate("Onboarding2")}}
+      <View
         style={{
-          padding: 4, 
-          backgroundColor: '#F8EBD3',
-          borderRadius: 30, 
-          borderWidth: 2, 
-          borderColor: 'black', 
-          justifyContent: 'center', 
-          alignItems: 'center',
-          width: 60,
-          height: 60,
-          marginRight: 10,
+          flexDirection: 'row',
+          marginHorizontal: 40,
+          marginTop: 15,
+          justifyContent: 'center',
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Onboarding2');
           }}
-          >
-        <AntDesign name="google" size={42} color="black" />
-      </TouchableOpacity>
+          style={{
+            padding: 4,
+            backgroundColor: '#F8EBD3',
+            borderRadius: 30,
+            borderWidth: 2,
+            borderColor: 'black',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 60,
+            height: 60,
+            marginRight: 10,
+          }}>
+          <AntDesign name="google" size={42} color="black" />
+        </TouchableOpacity>
 
-      <TouchableOpacity
-      onPress={()=>{navigation.navigate("Onboarding2")}}
-        style={{
-          padding: 6, 
-          backgroundColor: '#F8EBD3',
-          borderRadius: 30, 
-          borderWidth: 2, 
-          borderColor: 'black', 
-          justifyContent: 'center',  
-          alignItems: 'center',
-          width: 60,
-          height: 60,
-          marginRight: 10,
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Onboarding2');
           }}
-          >
-        <FontAwesome name="apple" size={42} color="black" />
-      </TouchableOpacity>
+          style={{
+            padding: 6,
+            backgroundColor: '#F8EBD3',
+            borderRadius: 30,
+            borderWidth: 2,
+            borderColor: 'black',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 60,
+            height: 60,
+            marginRight: 10,
+          }}>
+          <FontAwesome name="apple" size={42} color="black" />
+        </TouchableOpacity>
 
-      <TouchableOpacity
-      onPress={()=>{navigation.navigate("Onboarding2")}}
-        style={{
-          padding: 10, 
-          backgroundColor: '#F8EBD3',
-          borderRadius: 30, 
-          borderWidth: 2, 
-          borderColor: 'black', 
-          justifyContent: 'center',  
-          alignItems: 'center',
-          width: 60,
-          height: 60,
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Onboarding2');
           }}
-          >
-        <FontAwesome name="facebook" size={42} color="black" />
-      </TouchableOpacity>
-     </View>
-      
-
+          style={{
+            padding: 10,
+            backgroundColor: '#F8EBD3',
+            borderRadius: 30,
+            borderWidth: 2,
+            borderColor: 'black',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 60,
+            height: 60,
+          }}>
+          <FontAwesome name="facebook" size={42} color="black" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F8EBD3',
     fontFamily: 'Poppins',
+    fontWeight: '500',
   },
   toggleContainer: {
     flexDirection: 'row',
@@ -213,6 +269,5 @@ const styles = StyleSheet.create({
     color: 'white',
   },
 });
-
 
 export default GoferLogin;
