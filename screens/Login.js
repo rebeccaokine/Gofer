9bd34979914b8c35ff96620cf298b71e5e84cea3
Bex <rebeccaokine.na@gmail.com>
2023-08-04 23:26:44 +0000

    removed auth

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  SafeAreaView,
-  StyleSheet,
-  Pressable
-} from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView, StyleSheet, Pressable } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import FormTextInput from '../components/formTextInput';
@@ -23,126 +16,11 @@ const Login = ({ navigation }) => {
     }
   };
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   const handleLogin = () => {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Login successful, navigate to the appropriate screen
-        if (activeOption === 'gofer') {
-          navigation.navigate('GoferHome');
-        } else {
-          navigation.navigate('HirerHome');
-        }
-      })
-      .catch((error) => {
-        // Handle login errors (invalid credentials, network error, etc.)
-        console.log(error);
-      });
-  };
-
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await Google.logInAsync({
-        androidClientId: 'YOUR_ANDROID_CLIENT_ID',
-        iosClientId: 'YOUR_IOS_CLIENT_ID',
-        scopes: ['profile', 'email'],
-      });
-
-      if (result.type === 'success') {
-        const credential = firebase.auth.GoogleAuthProvider.credential(
-          result.idToken,
-          result.accessToken
-        );
-        firebase
-          .auth()
-          .signInWithCredential(credential)
-          .then((userCredential) => {
-            // Login successful, navigate to the appropriate screen
-            if (activeOption === 'gofer') {
-              navigation.navigate('GoferHome');
-            } else {
-              navigation.navigate('HirerHome');
-            }
-          })
-          .catch((error) => {
-            // Handle login errors
-            console.log(error);
-          });
-      }
-    } catch (e) {
-      console.log('Google Login Error:', e);
-    }
-  };
-
-  const handleAppleLogin = async () => {
-    try {
-      const credential = await AppleAuthentication.signInAsync({
-        requestedScopes: [AppleAuthentication.AppleAuthenticationScope.FULL_NAME, AppleAuthentication.AppleAuthenticationScope.EMAIL],
-      });
-
-      if (credential) {
-        const { identityToken, email, user } = credential;
-
-        const provider = new firebase.auth.OAuthProvider('apple.com');
-        const appleCredential = provider.credential({
-          idToken: identityToken,
-        });
-
-        firebase
-          .auth()
-          .signInWithCredential(appleCredential)
-          .then((userCredential) => {
-            // Login successful, navigate to the appropriate screen
-            if (activeOption === 'gofer') {
-              navigation.navigate('GoferHome');
-            } else {
-              navigation.navigate('HirerHome');
-            }
-          })
-          .catch((error) => {
-            // Handle login errors
-            console.log(error);
-          });
-      }
-    } catch (e) {
-      console.log('Apple Login Error:', e);
-    }
-  };
-
-  const handleFacebookLogin = async () => {
-    try {
-      await Facebook.initializeAsync({
-        appId: 'YOUR_FACEBOOK_APP_ID',
-      });
-
-      const { type, token } = await Facebook.logInWithReadPermissionsAsync({
-        permissions: ['public_profile', 'email'],
-      });
-
-      if (type === 'success') {
-        const credential = firebase.auth.FacebookAuthProvider.credential(token);
-        firebase
-          .auth()
-          .signInWithCredential(credential)
-          .then((userCredential) => {
-            // Login successful, navigate to the appropriate screen
-            if (activeOption === 'gofer') {
-              navigation.navigate('GoferHome');
-            } else {
-              navigation.navigate('HirerHome');
-            }
-          })
-          .catch((error) => {
-            // Handle login errors
-            console.log(error);
-          });
-      }
-    } catch ({ message }) {
-      console.log(`Facebook Login Error: ${message}`);
+    if (activeOption === 'gofer') {
+      navigation.navigate('GoferHome');
+    } else {
+      navigation.navigate('HirerHome');
     }
   };
 
@@ -150,30 +28,18 @@ const Login = ({ navigation }) => {
     <SafeAreaView style={styles.container}>
       <View style={styles.toggleContainer}>
         <TouchableOpacity
-          style={[
-            styles.button,
-            activeOption === 'gofer' && styles.activeButton,
-          ]}
-          onPress={handleToggle}>
-          <Text
-            style={[
-              styles.buttonText,
-              activeOption === 'gofer' && styles.activeButtonText,
-            ]}>
+          style={[styles.button, activeOption === 'gofer' && styles.activeButton]}
+          onPress={handleToggle}
+        >
+          <Text style={[styles.buttonText, activeOption === 'gofer' && styles.activeButtonText]}>
             Gofer
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={[
-            styles.button,
-            activeOption === 'hirer' && styles.activeButton,
-          ]}
-          onPress={handleToggle}>
-          <Text
-            style={[
-              styles.buttonText,
-              activeOption === 'hirer' && styles.activeButtonText,
-            ]}>
+          style={[styles.button, activeOption === 'hirer' && styles.activeButton]}
+          onPress={handleToggle}
+        >
+          <Text style={[styles.buttonText, activeOption === 'hirer' && styles.activeButtonText]}>
             Hirer
           </Text>
         </TouchableOpacity>
@@ -188,7 +54,8 @@ const Login = ({ navigation }) => {
             marginTop: 40,
             marginBottom: 30,
             textAlign: 'center',
-          }}>
+          }}
+        >
           Login
         </Text>
       </View>
@@ -198,29 +65,27 @@ const Login = ({ navigation }) => {
         <PasswordInput placeholder="Password" />
       </View>
 
-      <View
-        style={{
-          flexDirection: 'row',
-          marginHorizontal: 50,
-          alignSelf: 'flex-end',
-        }}>
-      <Pressable
+      <View style={{ flexDirection: 'row', marginHorizontal: 50, alignSelf: 'flex-end' }}>
+        <Pressable
           onPress={() => {
             navigation.navigate('ForgotPassword');
-          }}>
+          }}
+        >
           <Text
             style={{
-             fontSize: 16,
-            textAlign: 'center',
-            marginTop: 10,
-            fontFamily: 'Poppins-Regular',
-            }}>
-           {' '} Forgot Password?
+              fontSize: 16,
+              textAlign: 'center',
+              marginTop: 10,
+              fontFamily: 'Poppins-Regular',
+            }}
+          >
+            {' '}
+            Forgot Password?
           </Text>
         </Pressable>
       </View>
 
-       <TouchableOpacity
+      <TouchableOpacity
         onPress={handleLogin}
         style={{
           padding: 15,
@@ -233,13 +98,9 @@ const Login = ({ navigation }) => {
           fontFamily: 'Poppins-Medium',
           marginTop: 10,
           marginHorizontal: 40,
-        }}>
-        <Text
-          style={{
-            fontSize: 20,
-          }}>
-          Login
-        </Text>
+        }}
+      >
+        <Text style={{ fontSize: 20 }}>Login</Text>
       </TouchableOpacity>
 
       <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
@@ -250,21 +111,25 @@ const Login = ({ navigation }) => {
             textAlign: 'center',
             marginTop: 20,
             fontFamily: 'Poppins-Regular',
-          }}>
+          }}
+        >
           Don't have an account?
         </Text>
         <Pressable
           onPress={() => {
             navigation.navigate('Signup');
-          }}>
+          }}
+        >
           <Text
             style={{
-             fontSize: 16,
-            textAlign: 'center',
-            marginTop: 20,
-            fontFamily: 'Poppins-Regular',
-            }}>
-           {' '} Sign Up
+              fontSize: 16,
+              textAlign: 'center',
+              marginTop: 20,
+              fontFamily: 'Poppins-Regular',
+            }}
+          >
+            {' '}
+            Sign Up
           </Text>
         </Pressable>
       </View>
@@ -276,7 +141,8 @@ const Login = ({ navigation }) => {
           marginTop: 60,
           textAlign: 'center',
           fontFamily: 'Poppins-Regular',
-        }}>
+        }}
+      >
         Or connect with
       </Text>
 
@@ -286,7 +152,8 @@ const Login = ({ navigation }) => {
           marginHorizontal: 40,
           marginTop: 15,
           justifyContent: 'center',
-        }}>
+        }}
+      >
         <TouchableOpacity
           onPress={() => {
             navigation.navigate('Onboarding2');
@@ -302,7 +169,8 @@ const Login = ({ navigation }) => {
             width: 60,
             height: 60,
             marginRight: 10,
-          }}>
+          }}
+        >
           <AntDesign name="google" size={42} color="black" />
         </TouchableOpacity>
 
@@ -321,7 +189,8 @@ const Login = ({ navigation }) => {
             width: 60,
             height: 60,
             marginRight: 10,
-          }}>
+          }}
+        >
           <FontAwesome name="apple" size={42} color="black" />
         </TouchableOpacity>
 
@@ -339,7 +208,8 @@ const Login = ({ navigation }) => {
             alignItems: 'center',
             width: 60,
             height: 60,
-          }}>
+          }}
+        >
           <FontAwesome name="facebook" size={42} color="black" />
         </TouchableOpacity>
       </View>
@@ -350,7 +220,7 @@ const Login = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F8EBD3',    
+    backgroundColor: '#F8EBD3',
   },
   toggleContainer: {
     flexDirection: 'row',
