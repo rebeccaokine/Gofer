e75d65febab5cfde06d958acaffe0d39388dc0c3
Bex <rebeccaokine.na@gmail.com>
2023-08-17 20:53:53 +0000

    update

--- screens/Gofer/GoferHome.js
+++ screens/Gofer/GoferHome.js
@@ -1,128 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import NavigationBar from '../../components/navigationBar';
 import SearchButton from '../../components/searchButton';
 import Categories from '../../components/categories';
 import Suggested from '../../components/suggested';
 import { Ionicons } from '@expo/vector-icons';
 import { Octicons } from '@expo/vector-icons';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-const GoferHome = ({navigation}) => {
+const GoferHome = ({ navigation }) => {
   const [userName, setUserName] = useState('');
 
   useEffect(() => {
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is logged in, set the user's name
-        console.log("User Display Name:", user.displayName);
-        setUserName(user.displayName);
-      }
-    });
+    // Simulating Firebase auth state change
+    const fakeUser = { displayName: 'Rebecca Okine' };
 
+    // User is logged in, set the user's name
+    setUserName(fakeUser.displayName);
+
+    // Unsubscribe when the component is unmounted
     return () => {
-      // Unsubscribe when the component is unmounted
-      unsubscribe();
+      // Cleanup if needed
     };
   }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          flex: 1,
-          marginVertical: 40,
-          marginHorizontal: 20,
-        }}>
-
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-          }}>
-          <Text
-            style={{
-              fontSize: 24,
-              fontFamily: 'Poppins-SemiBold',
-            }}>
-            Hello, {userName}
-          </Text>
-
+      <View style={styles.mainContainer}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>Hello, {userName}</Text>
           <TouchableOpacity
             onPress={() => {
               navigation.navigate('Onboarding2');
             }}
-            style={{
-            }}>
-            <Ionicons
-              name="notifications-circle-outline"
-              size={37}
-              color="black"
-            />
+          >
+            <Ionicons name="notifications-circle-outline" size={37} color="black" />
           </TouchableOpacity>
         </View>
 
-        <View
-          style={{
-            flexDirection: 'row',
-          }}>
+        <View style={styles.locationContainer}>
           <Octicons name="location" size={20} color="black" />
-          <Text
-            style={{
-              fontSize: 18,
-              fontFamily: 'Poppins-Medium',
-            }}>
-            {' '}
-            Accra, Ghana{' '}
-          </Text>
+          <Text style={styles.locationText}>Accra, Ghana</Text>
         </View>
 
-        <SearchButton placeholder="Find an errand" />
       </View>
-      <View
-        style={{
-          flex: 3,
-          marginLeft: 20,
-        }}>
-        <Text
-          style={{
-            fontFamily: 'Poppins-SemiBold',
-            fontSize: 22,
-            marginTop: 15,
-            marginBottom: 5,
-          }}>
-          {' '}
-          Categories{' '}
-        </Text>
+      <View style={styles.categoriesContainer}>
+        <Text style={styles.categoriesHeader}>Categories</Text>
         <Categories navigation={navigation} />
       </View>
 
-      <View
-        style={{
-          flex: 4,
-          marginLeft: 20,
-        }}>
-        <Text
-          style={{
-            fontFamily: 'Poppins-SemiBold',
-            fontSize: 22,
-            marginBottom: 10,
-            marginTop: 10,
-          }}>
-          Suggested for you
-        </Text>
-        <Suggested navigation={navigation}/>
+      <View style={styles.suggestedContainer}>
+        <Text style={styles.suggestedHeader}>Suggested for you</Text>
+        <Suggested navigation={navigation} />
       </View>
 
-      <NavigationBar/>
+      <NavigationBar />
     </SafeAreaView>
   );
 };
@@ -131,6 +62,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F8EBD3',
+  },
+  mainContainer: {
+    flex: 2,
+    marginVertical: 40,
+    marginHorizontal: 20,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontFamily: 'Poppins-SemiBold',
+  },
+  locationContainer: {
+    flexDirection: 'row',
+  },
+  locationText: {
+    fontSize: 18,
+    fontFamily: 'Poppins-Medium',
+  },
+  categoriesContainer: {
+    flex: 14,
+    marginLeft: 20,
+  },
+  categoriesHeader: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 22,
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  suggestedContainer: {
+    flex: 18,
+    marginLeft: 20,
+  },
+  suggestedHeader: {
+    fontFamily: 'Poppins-SemiBold',
+    fontSize: 22,
+    marginBottom: 10,
+    marginTop: 10,
   },
 });
 
