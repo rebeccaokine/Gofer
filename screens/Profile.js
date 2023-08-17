7cf569912d926b7f4ba5b302ac634ba98e0b5aa3
Bex <rebeccaokine.na@gmail.com>
2023-08-17 23:28:18 +0000

    update

--- screens/Profile.js
+++ screens/Profile.js
@@ -6,7 +6,6 @@ import {
   StyleSheet,
   TouchableOpacity,
   Image,
-  Pressable,
 } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { Octicons } from '@expo/vector-icons';
@@ -44,151 +43,67 @@ const Profile = ({ navigation }) => {
             style={{
               fontSize: 24,
               fontFamily: 'Poppins-Medium',
+              marginBottom: 20,
             }}>
             Dereck Griffin
           </Text>
         </View>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            marginTop: 40,
-            padding: 15,
-          }}>
-          <View
-            style={{
-              flexDirection: 'column',
-            }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('EditProfile');
+          }}
+          style={styles.optionContainer}>
+          <View style={styles.optionIconContainer}>
             <Octicons name="pencil" size={24} color="black" />
           </View>
-          <View style={{ flexDirection: 'column' }}>
-            <Pressable
-              onPress={() => {
-                navigation.navigate('EditProfile');
-              }}>
-              <Text
-                style={{
-                  fontSize: 20,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {'  '}
-                Edit Profile
-              </Text>
-            </Pressable>
-          </View>
-        </View>
+          <Text style={styles.optionText}>Edit Profile</Text>
+        </TouchableOpacity>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            padding: 15,
-          }}>
-          <View
-            style={{
-              flexDirection: 'column',
-            }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('VerifyProfile');
+          }}
+          style={styles.optionContainer}>
+          <View style={styles.optionIconContainer}>
             <FontAwesome5 name="check-circle" size={24} color="black" />
           </View>
-          <View style={{ flexDirection: 'column' }}>
-            <Pressable
-              onPress={() => {
-                navigation.navigate('VerifyProfile');
-              }}>
-              <Text
-                style={{
-                  fontSize: 20,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {'  '}
-                Verify Profile
-              </Text>
-            </Pressable>
-          </View>
-        </View>
+          <Text style={styles.optionText}>Verify Profile</Text>
+        </TouchableOpacity>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            padding: 15,
-          }}>
-          <View
-            style={{
-              flexDirection: 'column',
-            }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Login');
+          }}
+          style={styles.optionContainer}>
+          <View style={styles.optionIconContainer}>
             <Ionicons name="card-outline" size={26} color="black" />
           </View>
-          <View style={{ flexDirection: 'column' }}>
-            <Pressable
-              onPress={() => {
-                navigation.navigate('Login');
-              }}>
-              <Text
-                style={{
-                  fontSize: 20,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {'  '}
-                View Payments
-              </Text>
-            </Pressable>
-          </View>
-        </View>
+          <Text style={styles.optionText}>View Payments</Text>
+        </TouchableOpacity>
 
-        <View
-          style={{
-            flexDirection: 'row',
-            padding: 15,
-          }}>
-          <View
-            style={{
-              flexDirection: 'column',
-            }}>
-            <Ionicons name="card-outline" size={26} color="black" />
-          </View>
-          <View style={{ flexDirection: 'column' }}>
-            <Pressable
-              onPress={() => {
-                navigation.navigate('Login');
-              }}>
-              <Text
-                style={{
-                  fontSize: 20,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {'  '}
-                Change Payment Preference
-              </Text>
-            </Pressable>
-          </View>
-        </View>
-
-        <View
-          style={{
-            flexDirection: 'row',
-            padding: 15,
-          }}>
-          <View
-            style={{
-              flexDirection: 'column',
-            }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Login');
+          }}
+          style={styles.optionContainer}>
+          <View style={styles.optionIconContainer}>
             <SimpleLineIcons name="settings" size={24} color="black" />
           </View>
-          <View style={{ flexDirection: 'column' }}>
-            <Pressable
-              onPress={() => {
-                navigation.navigate('Login');
-              }}>
-              <Text
-                style={{
-                  fontSize: 20,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {'  '}
-                Settings
-              </Text>
-            </Pressable>
+          <Text style={styles.optionText}>Settings</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Login');
+          }}
+          style={styles.optionContainer}>
+          <View style={styles.optionIconContainer}>
+            <AntDesign name="logout" size={24} color="black" />
           </View>
-        </View>
+          <Text style={styles.optionText}>Logout</Text>
+        </TouchableOpacity>
+
         <View style={styles.details}></View>
       </View>
     </SafeAreaView>
@@ -221,10 +136,24 @@ const styles = StyleSheet.create({
     marginLeft: 45,
     marginVertical: 20,
   },
-  details: {
-    flex: 1,
-    marginTop: 20,
-    marginHorizontal: 10,
+  optionContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+    padding: 10,
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 30,
+    marginHorizontal: 20,
+  },
+  optionIconContainer: {
+    flexDirection: 'column',
+    marginLeft: 10,
+  },
+  optionText: {
+    fontSize: 20,
+    fontFamily: 'Poppins-Regular',
+    marginLeft: 15,
   },
   image: {
     alignSelf: 'center',
