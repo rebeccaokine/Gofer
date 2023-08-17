49d8c8108616615d5a0e7d801ce4ba519d471205
Bex <rebeccaokine.na@gmail.com>
2023-08-17 23:34:38 +0000

    update

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -123,6 +123,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontFamily: 'Poppins-Medium',
     marginLeft: 80,
+    marginVertical: 40,
   },
   chatContainer: {
     paddingVertical: 10,
