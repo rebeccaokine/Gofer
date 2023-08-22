e2938b208fb021e77abf98439fef3c00a41ef264
Bex <rebeccaokine.na@gmail.com>
2023-08-22 22:17:16 +0000

    update chatscreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,73 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image, FlatList, SafeAreaView, TouchableOpacity, TextInput } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { firebase } from '../firebaseConfig'; 
+import 'firebase/firestore';
 
-const ChatScreen = ({ route, navigation }) => {
-  const { message } = route.params;
-
+const ChatScreen = ({ navigation }) => {
+  const db = firebase.firestore();
+ 
   const [inputText, setInputText] = useState('');
-  const [chats, setChats] = useState([
-    {
-      id: 1,
-      sender: {
-        name: 'John Doe',
-        avatar: require('../assets/avatar.jpeg'),
-      },
-      message: 'Hello, how are you?',
-      timestamp: '10:00 AM',
-    },
-    {
-      id: 2,
-      sender: {
-        name: 'Jane Smith',
-        avatar: require('../assets/avatar.jpeg'),
-      },
-      message: 'I am doing great!',
-      timestamp: '10:05 AM',
-    },
-    
-  ]);
+  const [chats, setChats] = useState([]);
 
-  const handleSend = () => {
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('chats')
+      .orderBy('timestamp')
+      .onSnapshot((snapshot) => {
+        const newChats = snapshot.docs.map((doc) => {
+          const chatData = doc.data();
+          return {
+            ...chatData,
+            timestamp: chatData.timestamp.toDate(), // Convert Firestore timestamp to JavaScript Date object
+          };
+        });
+        setChats(newChats);
+      });
+  
+    return () => unsubscribe();
+  }, []);
+
+  const handleSend = async () => {
     if (inputText.trim() === '') {
       return;
     }
 
     const newChat = {
-      id: chats.length + 1,
-      sender: message.sender,
+      sender: 'John Doe', 
       message: inputText.trim(),
-      timestamp: getCurrentTimestamp(),
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     };
 
-    setChats([...chats, newChat]);
-    setInputText('');
-  };
-
-  const getCurrentTimestamp = () => {
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const formattedHours = hours < 10 ? `0${hours}` : hours;
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-
-    return `${formattedHours}:${formattedMinutes}`;
+    try {
+      await db.collection('chats').add(newChat);
+      setInputText('');
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
   };
 
   const renderItem = ({ item }) => (
     <View
       style={[
         styles.messageContainer,
-        item.sender.name === message.sender.name ? styles.rightMessageContainer : styles.leftMessageContainer,
+        styles.rightMessageContainer,
       ]}
     >
-      {item.sender.name !== message.sender.name && (
-        <Image source={item.sender.avatar} style={styles.avatar} />
-      )}
       <View style={styles.messageContent}>
-        {item.sender.name !== message.sender.name && (
-          <Text style={styles.senderName}>{item.sender.name}</Text>
-        )}
         <Text style={styles.messageText}>{item.message}</Text>
         <Text style={styles.timestamp}>{item.timestamp}</Text>
       </View>
@@ -76,31 +63,31 @@ const ChatScreen = ({ route, navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-          <AntDesign name="leftcircleo" size={37} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.title}>{message.sender.name}</Text>
-      </View>
-      <FlatList
-        data={chats}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={renderItem}
-        inverted
-        contentContainerStyle={styles.chatContainer}
+    <View style={styles.header}>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+        <AntDesign name="leftcircleo" size={37} color="black" />
+      </TouchableOpacity>
+      <Text style={styles.title}>Chat</Text>
+    </View>
+    <FlatList
+      data={chats}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderItem}
+      inverted
+      contentContainerStyle={styles.chatContainer}
+    />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        placeholder="Type a message..."
+        value={inputText}
+        onChangeText={(text) => setInputText(text)}
       />
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Type a message..."
-          value={inputText}
-          onChangeText={(text) => setInputText(text)}
-        />
-        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
-          <AntDesign name="rightcircle" size={30} color="black" />
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+      <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+        <AntDesign name="rightcircle" size={30} color="black" />
+      </TouchableOpacity>
+    </View>
+  </SafeAreaView>
   );
 };
 
@@ -122,7 +109,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontFamily: 'Poppins-Medium',
-    marginLeft: 80,
+    marginLeft: 110,
     marginVertical: 40,
   },
   chatContainer: {
