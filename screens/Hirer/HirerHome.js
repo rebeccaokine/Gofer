c19cc9e234ac34cec1346cda175b243550aec107
Bex <rebeccaokine.na@gmail.com>
2023-08-24 03:55:04 +0000

    update

--- screens/Hirer/HirerHome.js
+++ screens/Hirer/HirerHome.js
@@ -11,22 +11,57 @@ import { Ionicons } from '@expo/vector-icons';
 import { Octicons } from '@expo/vector-icons';
 import UpcomingHirerErrands from '../../components/upcomingHirerErrands';
 import OngoingErrands from '../../components/ongoingErrands';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
 const HirerHome = ({ navigation, route }) => {
   const [userName, setUserName] = useState('');
+  const [userLocation, setUserLocation] = useState(null);
+  const [address, setAddress] = useState('');
 
   useEffect(() => {
-    // Retrieve the displayName from navigation parameters
     const displayName = route.params?.displayName || '';
-
-    // Set the retrieved displayName as the user's name
     setUserName(displayName);
 
-    // Unsubscribe when the component is unmounted
+    const requestLocationPermission = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status === 'granted') {
+        const location = await Location.getCurrentPositionAsync({});
+        setUserLocation(location);
+
+        const apiKey = 'AIzaSyCoTiHaUtOLEJ4QHK1AN0Jlqn3B0_pMSc4'; 
+
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.coords.latitude},${location.coords.longitude}&key=${apiKey}`
+        );
+       
+        if (response.data.results.length > 0) {
+          const addressComponents = response.data.results[0].address_components;
+          const cityComponent = addressComponents.find(
+            (component) => component.types.includes('locality')
+          );
+          const countryComponent = addressComponents.find(
+            (component) => component.types.includes('country')
+          );
+          
+          const city = cityComponent ? cityComponent.long_name : '';
+          const country = countryComponent ? countryComponent.long_name : '';
+          
+          setAddress(`${city}, ${country}`);
+        }
+      } else {
+        console.error('Error requesting location permission');
+      }
+    };
+
+    requestLocationPermission();
+
     return () => {
       // Cleanup if needed
     };
   }, [route.params?.displayName]);
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -44,7 +79,7 @@ const HirerHome = ({ navigation, route }) => {
 
         <View style={styles.locationContainer}>
           <Octicons name="location" size={20} color="black" />
-          <Text style={styles.locationText}> Accra, Ghana</Text>
+          <Text style={styles.locationText}> {address}</Text>
         </View>
 
       </View>
