7c46f93bbb3385822f9fe53620c3c5c512a4dad4
Bex <rebeccaokine.na@gmail.com>
2023-08-25 16:48:27 +0000

    update

--- components/suggested.js
+++ components/suggested.js
@@ -1,463 +1,87 @@
-import React from 'react';
-import {
-  View,
-  TouchableOpacity,
-  ScrollView,
-  Text,
-  Image,
-  StyleSheet,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TouchableOpacity, ScrollView, Text, Image, StyleSheet } from 'react-native';
 import { Octicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { firebase } from '../firebaseConfig';
+import 'firebase/firestore';
 
-const Suggested = ({navigation}) => {
+
+const Suggested = ({ navigation }) => {
+  const [suggestedErrands, setSuggestedErrands] = useState([]);
+
+  const categoryImageMappings = {
+    'Home Cleaning': require('../assets/cleaning.png'),
+    'Laundry': require('../assets/laundry-machine.png'),
+    'Cooking': require('../assets/cooking.png'),
+    'Babysitting': require('../assets/babysitting.png'),
+    'Delivery': require('../assets/delivery.png'),
+    'Pet Care': require('../assets/animal-care.png'),
+    'Grocery Shopping': require('../assets/grocery.png'),
+  };
+
+  useEffect(() => {
+    const fetchSuggestedErrands = async () => {
+      try {
+        const snapshot = await firebase.firestore().collection('createErrand').get();
+        const allErrands = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        const randomIndices = generateRandomIndices(allErrands.length, 5);
+        const selectedErrands = randomIndices.map((index) => allErrands[index]);
+        setSuggestedErrands(selectedErrands);
+      } catch (error) {
+        console.error('Error fetching errands:', error);
+      }
+    };
+
+    fetchSuggestedErrands();
+  }, []);
+
+  const generateRandomIndices = (max, count) => {
+    const indices = [];
+    while (indices.length < count) {
+      const randomIndex = Math.floor(Math.random() * max);
+      if (!indices.includes(randomIndex)) {
+        indices.push(randomIndex);
+      }
+    }
+    return indices;
+  };
+
   return (
     <ScrollView showsHorizontalScrollIndicator={false} horizontal>
       <View style={styles.container}>
-        <View
-          style={{
-            flexDirection: 'column',
-            width: 220,
-            height: 290,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderWidth: 2,
-            borderRadius: 20,
-            backgroundColor: 'white',
-          }}>
-          <View
-            style={{
-              width: 200,
-              height: 140,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderWidth: 2,
-              borderRadius: 15,
-              backgroundColor: 'rgba(0, 178, 255, 0.50)',
-              marginHorizontal: 9,
-              marginTop: 20,
-              marginBottom: 5,
-            }}>
-            <Image
-              source={require('../assets/laundry-machine.png')}
-              style={styles.image}
-            />
-          </View>
-          <View
-            style={{
-              marginHorizontal: 10,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text
-              style={{
-                fontSize: 18,
-                fontFamily: 'Poppins-Medium',
-              }}>
-              Wash my clothes
-            </Text>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Octicons name="location" size={18} color="black" />
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {' '}
-                Mile 7, Achimota
-              </Text>
+        {suggestedErrands.map((errand) => (
+          <View key={errand.id} style={styles.cardContainer}>
+            <View style={styles.imageContainer}>
+            <Image source={categoryImageMappings[errand.category]} style={styles.image} />
             </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Medium',
-                  color: '#00B2FF',
-                }}>
-                GH₵
-              </Text>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Medium',
-                  marginLeft: 5,
-                  marginRight: 20,
-                  color: '#00B2FF',
-                }}>
-                200
-              </Text>
-
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <AntDesign name="star" size={18} color="#FFA800" />
-                <Text
-                  style={{
-                    fontSize: 16,
-                    fontFamily: 'Poppins-Medium',
-                    marginLeft: 5,
-                    color: 'gray',
-                  }}>
-                  (3.2)
-                </Text>
+            <View style={styles.detailsContainer}>
+              <Text style={styles.title}>{errand.title}</Text>
+              <View style={styles.locationRow}>
+                <Octicons name="location" size={18} color="black" />
+                <Text style={styles.location}>{errand.location}</Text>
+              </View>
+              <View style={styles.priceRow}>
+                <Text style={styles.price}>GH₵  {errand.price}</Text>
+              </View>
+              <View style={styles.buttonRow}>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('ErrandDetails', { errandId : errand.id });
+                  }}
+                  style={styles.detailsButton}>
+                  <Text>Details</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate('BookingConfirmation', {  });
+                  }}
+                  style={styles.bookButton}>
+                  <Text>Book</Text>
+                </TouchableOpacity>
               </View>
             </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                paddingBottom: 20,
-                marginTop: 5,
-              }}>
-              <TouchableOpacity
-               onPress={()=>{navigation.navigate("ErrandDetails")}}
-                style={{
-                  borderWidth: 2,
-                  borderRadius: 15,
-                  backgroundColor: 'white',
-                  paddingHorizontal: 20,
-                  paddingVertical: 5,
-                  marginRight: 10,
-                }}>
-                <View>
-                  <Text>Details</Text>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('BookingConfirmation');
-                }}
-                style={{
-                  borderWidth: 2,
-                  borderRadius: 15,
-                  backgroundColor: 'white',
-                  paddingHorizontal: 20,
-                  paddingVertical: 5,
-                }}>
-                <View>
-                  <Text>Book</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
           </View>
-        </View>
-
-        {/**second view**/}
-
-        <View
-          style={{
-            flexDirection: 'column',
-            width: 220,
-            height: 290,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderWidth: 2,
-            borderRadius: 20,
-            backgroundColor: 'white',
-            marginLeft: 10,
-          }}>
-          <View
-            style={{
-              width: 200,
-              height: 140,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderWidth: 2,
-              borderRadius: 15,
-              backgroundColor: 'rgba(0, 178, 255, 0.50)',
-              marginHorizontal: 9,
-              marginTop: 20,
-              marginBottom: 5,
-            }}>
-            <Image
-              source={require('../assets/laundry-machine.png')}
-              style={styles.image}
-            />
-          </View>
-          <View
-            style={{
-              marginHorizontal: 10,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text
-              style={{
-                fontSize: 18,
-                fontFamily: 'Poppins-Medium',
-              }}>
-              Wash my clothes
-            </Text>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Octicons name="location" size={18} color="black" />
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {' '}
-                Mile 7, Achimota
-              </Text>
-            </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Medium',
-                  color: '#00B2FF',
-                }}>
-                GH₵
-              </Text>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Medium',
-                  marginLeft: 5,
-                  marginRight: 20,
-                  color: '#00B2FF',
-                }}>
-                200
-              </Text>
-
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <AntDesign name="star" size={18} color="#FFA800" />
-                <Text
-                  style={{
-                    fontSize: 16,
-                    fontFamily: 'Poppins-Medium',
-                    marginLeft: 5,
-                    color: 'gray',
-                  }}>
-                  (3.2)
-                </Text>
-              </View>
-            </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                paddingBottom: 20,
-                marginTop: 5,
-              }}>
-              <TouchableOpacity
-               onPress={()=>{navigation.navigate("ErrandDetails")}}
-                style={{
-                  borderWidth: 2,
-                  borderRadius: 15,
-                  backgroundColor: 'white',
-                  paddingHorizontal: 20,
-                  paddingVertical: 5,
-                  marginRight: 10,
-                }}>
-                <View>
-                  <Text>Details</Text>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('BookingConfirmation');
-                }}
-                style={{
-                  borderWidth: 2,
-                  borderRadius: 15,
-                  backgroundColor: 'white',
-                  paddingHorizontal: 20,
-                  paddingVertical: 5,
-                }}>
-                <View>
-                  <Text>Book</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-
-        {/**third view */}
-        <View
-          style={{
-            flexDirection: 'column',
-            width: 220,
-            height: 290,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderWidth: 2,
-            borderRadius: 20,
-            backgroundColor: 'white',
-            marginLeft: 10,
-          }}>
-          <View
-            style={{
-              width: 200,
-              height: 140,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderWidth: 2,
-              borderRadius: 15,
-              backgroundColor: 'rgba(0, 178, 255, 0.50)',
-              marginHorizontal: 9,
-              marginTop: 20,
-              marginBottom: 5,
-            }}>
-            <Image
-              source={require('../assets/laundry-machine.png')}
-              style={styles.image}
-            />
-          </View>
-          <View
-            style={{
-              marginHorizontal: 10,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text
-              style={{
-                fontSize: 18,
-                fontFamily: 'Poppins-Medium',
-              }}>
-              Wash my clothes
-            </Text>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Octicons name="location" size={18} color="black" />
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Regular',
-                }}>
-                {' '}
-                Mile 7, Achimota
-              </Text>
-            </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Medium',
-                  color: '#00B2FF',
-                }}>
-                GH₵
-              </Text>
-              <Text
-                style={{
-                  fontSize: 16,
-                  fontFamily: 'Poppins-Medium',
-                  marginLeft: 5,
-                  marginRight: 20,
-                  color: '#00B2FF',
-                }}>
-                200
-              </Text>
-
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}>
-                <AntDesign name="star" size={18} color="#FFA800" />
-                <Text
-                  style={{
-                    fontSize: 16,
-                    fontFamily: 'Poppins-Medium',
-                    marginLeft: 5,
-                    color: 'gray',
-                  }}>
-                  (3.2)
-                </Text>
-              </View>
-            </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                paddingBottom: 20,
-                marginTop: 5,
-              }}>
-              <TouchableOpacity
-               onPress={()=>{navigation.navigate("ErrandDetails")}}
-                style={{
-                  borderWidth: 2,
-                  borderRadius: 15,
-                  backgroundColor: 'white',
-                  paddingHorizontal: 20,
-                  paddingVertical: 5,
-                  marginRight: 10,
-                }}>
-                <View>
-                  <Text>Details</Text>
-                </View>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.navigate('BookingConfirmation');
-                }}
-                style={{
-                  borderWidth: 2,
-                  borderRadius: 15,
-                  backgroundColor: 'white',
-                  paddingHorizontal: 20,
-                  paddingVertical: 5,
-                }}>
-                <View>
-                  <Text>Book</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-
-        {/**end of cards */}
+        ))}
       </View>
     </ScrollView>
   );
@@ -467,9 +91,95 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
   },
+  cardContainer: {
+    width: 220,
+    height: 290,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    marginLeft: 10,
+  },
+  imageContainer: {
+    width: 200,
+    height: 140,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderRadius: 15,
+    backgroundColor: 'rgba(0, 178, 255, 0.50)',
+    marginHorizontal: 9,
+    marginTop: 20,
+    marginBottom: 5,
+  },
   image: {
     width: 120,
     height: 120,
+  },
+  detailsContainer: {
+    marginHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: 'Poppins-Medium',
+  },
+  locationRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  location: {
+    fontSize: 16,
+    fontFamily: 'Poppins-Regular',
+    marginLeft: 5,
+  },
+  priceRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 4,
+  },
+  price: {
+    fontSize: 16,
+    fontFamily: 'Poppins-Medium',
+    color: '#00B2FF',
+    marginRight: 20,
+  },
+  starRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rating: {
+    fontSize: 16,
+    fontFamily: 'Poppins-Medium',
+    marginLeft: 5,
+    color: 'gray',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 20,
+    marginTop: 5,
+  },
+  detailsButton: {
+    borderWidth: 2,
+    borderRadius: 15,
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+  bookButton: {
+    borderWidth: 2,
+    borderRadius: 15,
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
   },
 });
 
