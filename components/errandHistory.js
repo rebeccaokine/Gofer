f91298532dc2799407f3be8a5aef2cd803a47a67
Bex <rebeccaokine.na@gmail.com>
2023-08-28 04:28:12 +0000

    Update the components/errandhistory.js to add new flow

--- components/errandHistory.js
+++ components/errandHistory.js
@@ -1,151 +1,45 @@
-import React from 'react';
-import {
-  View,
-  ScrollView,
-  Text,
-  Image,
-  StyleSheet,
-} from 'react-native';
-import { Octicons } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import React from "react";
+import { View, ScrollView, Text, Image, StyleSheet } from "react-native";
+import { Octicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 const ErrandHistory = () => {
   return (
     <ScrollView showsVerticalScrollIndicator={false} vertical>
       <View style={styles.container}>
         {/* Start of the first view */}
-        <View >
-          {/* Date and Status */}
-          <View
-            style={{
-              flexDirection: 'row',
-              marginBottom: 10,
-              marginHorizontal: 10,
-              justifyContent: 'space-between',
-            }}>
-            <Text
-              style={{
-                fontSize: 18,
-                fontFamily: 'Poppins-Medium',
-              }}>
-              Monday, 20th August
-            </Text>
-            <Text
-              style={{
-                fontSize: 18,
-                color: '#26AD5C',
-                fontFamily: 'Poppins-Medium',
-              }}>
-              {' '}
-              Completed
-            </Text>
-          </View>
+        {/* Date and Status */}
+        <View style={[styles.row, styles.spaceBetween]}>
+          <Text style={styles.date}>Monday, 20th August</Text>
+          <Text style={[styles.date, styles.greenText]}>Completed</Text>
+        </View>
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
           {/* Main content */}
-          <View style={{alignItems: 'center'}}>
-            <View
-              style={{
-                flexDirection: 'row',
-                width: 350,
-                height: 130,
-                alignItems: 'center',
-                justifyContent: 'center',
-                borderWidth: 2,
-                borderRadius: 20,
-                backgroundColor: 'white',
-                marginBottom: 10,
-              }}>
-              {/* Image */}
-              <View
-                style={{
-                  width: 130,
-                  height: 110,
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  borderWidth: 2,
-                  borderRadius: 15,
-                  backgroundColor: 'rgba(0, 178, 255, 0.50)',
-                  marginRight: 15,
-                }}>
-                <Image
-                  source={require('../assets/laundry-machine.png')}
-                  style={styles.image}
-                />
+          <View style={styles.cardContent}>
+            {/* Image */}
+            <View style={styles.imageContainer}>
+              <Image
+                source={require("../assets/laundry-machine.png")}
+                style={styles.image}
+              />
+            </View>
+            {/* Text Content */}
+            <View style={styles.textContainer}>
+              {/* Task Title */}
+              <Text style={styles.title}>Wash my clothes</Text>
+
+              {/* Location */}
+              <View style={[styles.row, styles.locationRow]}>
+                <Octicons name="location" size={20} color="black" />
+                <Text style={styles.location}> Mile 7, Achimota</Text>
               </View>
-              {/* Text Content */}
-              <View
-                style={{
-                  flexDirection: 'column',
-                  alignItems: 'left',
-                  justifyContent: 'center',
-                  marginRight: 10,
-                }}>
-                {/* Task Title */}
-                <Text
-                  style={{
-                    fontSize: 20,
-                    fontFamily: 'Poppins-Medium',
-                  }}>
-                  Wash my clothes
-                </Text>
 
-                {/* Location */}
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    marginTop: 4,
-                  }}>
-                  <Octicons name="location" size={20} color="black" />
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: '400',
-                    }}>
-                    {' '}
-                    Mile 7, Achimota
-                  </Text>
-                </View>
-
-                {/* Price and Rating */}
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    marginTop: 5,
-                  }}>
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontFamily: 'Poppins-Medium',
-                      color: '#00B2FF',
-                    }}>
-                    GH₵
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontFamily: 'Poppins-Medium',
-                      marginLeft: 5,
-                      marginRight: 20,
-                      color: '#00B2FF',
-                    }}>
-                    200
-                  </Text>
-
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                    }}>
-                    <AntDesign name="star" size={20} color="#FFA800" />
-                    <Text
-                      style={{
-                        fontSize: 16,
-                        fontFamily: 'Poppins-Medium',
-                        marginLeft: 5,
-                        color: 'gray',
-                      }}>
-                      (3.2)
-                    </Text>
-                  </View>
+              {/* Price and Rating */}
+              <View style={[styles.row, styles.priceRow]}>
+                <Text style={[styles.price, styles.blueText]}>GH₵ 200</Text>
+                <View style={styles.row}>
+                  <AntDesign name="star" size={20} color="#FFA800" />
+                  <Text style={styles.rating}>(3.2)</Text>
                 </View>
               </View>
             </View>
@@ -158,11 +52,90 @@ const ErrandHistory = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
+    flexDirection: "column",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  spaceBetween: {
+    justifyContent: "space-between",
+  },
+  date: {
+    fontSize: 18,
+    fontFamily: "Poppins-Medium",
+  },
+  greenText: {
+    color: "#26AD5C",
+  },
+  cardContainer: {
+    borderWidth: 2,
+    borderRadius: 20,
+    backgroundColor: "white",
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardContent: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    width: 350,
+    height: 130,
+    borderWidth: 2,
+    borderRadius: 20,
+    backgroundColor: "white",
+    marginBottom: 10,
+  },
+  imageContainer: {
+    width: 120,
+    height: 110,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 2,
+    borderRadius: 15,
+    backgroundColor: "rgba(0, 178, 255, 0.50)",
+    marginRight: 5,
   },
   image: {
     width: 90,
     height: 90,
+  },
+  textContainer: {
+    flexDirection: "column",
+    alignItems: "left",
+    justifyContent: "center",
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "Poppins-Medium",
+  },
+  locationRow: {
+    marginTop: 4,
+  },
+  location: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  priceRow: {
+    marginTop: 5,
+  },
+  price: {
+    fontSize: 16,
+    fontFamily: "Poppins-Medium",
+    marginLeft: 5,
+    marginRight: 20,
+  },
+  blueText: {
+    color: "#00B2FF",
+  },
+  rating: {
+    fontSize: 16,
+    fontFamily: "Poppins-Medium",
+    marginLeft: 5,
+    color: "gray",
   },
 });
 
